b1c02056ea85b98894a595674e02b667976c5d63
ronald <wedema@home.nl>
2021-09-17 10:05:43 +0200

    remove test

--- AssessmentPrepProgramming/sub_module.py
+++
@@ -1,9 +0,0 @@
-def my_own_function(to_be_tested):
-    """
-    This function is to be used from the other module
-    :return:
-    """
-
-    my_own_funtion_return_value = len(to_be_tested)
-
-    return my_own_funtion_return_value
